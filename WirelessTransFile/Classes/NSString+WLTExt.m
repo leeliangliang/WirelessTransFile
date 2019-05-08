5294c3acdad0fb1699400611bfdfdc2abb9e27a0
亮 李 <554166037@qq.com>
2019-05-08 23:31:56 +0800

    添加文件大小计算

--- WirelessTransFile/Classes/NSString+WLTExt.m
+++ WirelessTransFile/Classes/NSString+WLTExt.m
@@ -15,7 +15,12 @@ const unsigned long long kBitSize_KB_TAG = (unsigned long long)1024;
 + (NSString *)formatBitSizeWith:(unsigned long long)size
 {
     if (size >= kBitSize_TB_TAG) {
-        return [NSString stringWithFormat:@"%.1f",size/kBitSize_TB_TAG];
+        
+        NSDecimalNumberHandler *roundUp = [NSDecimalNumberHandler decimalNumberHandlerWithRoundingMode:NSRoundPlain scale:2 raiseOnExactness:NO raiseOnOverflow:NO raiseOnUnderflow:NO raiseOnDivideByZero:YES];
+        NSDecimalNumber *sizeNum = [[NSDecimalNumber alloc]initWithUnsignedLongLong:size];
+        NSDecimalNumber *tb_tag = [[NSDecimalNumber alloc]initWithUnsignedLongLong:kBitSize_TB_TAG];
+        NSDecimalNumber *result = [tb_tag decimalNumberByDividingBy:sizeNum withBehavior:roundUp];
+        return [NSString stringWithFormat:@"%.1f", [result floatValue]];
     }
     return nil;
 }
